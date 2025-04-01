5d79b14f918175a9423960f022632c539fe9b251
julienmalard <julien.malard@mail.mcgill.ca>
2025-04-01 17:02:47 +0200

    எண் வடிவூட்டம்

--- src/தன்விவரங்கள்/உருப்படி/எண்.py
+++ src/தன்விவரங்கள்/உருப்படி/எண்.py
@@ -1,8 +1,9 @@
 from typing import Iterable
 
 from odfdo import Span
-
+from எண்ணிக்கை import உரைக்கு
 from .உரை import உரை
+from .வெளியீடுகள்.கருவிகள் import wuj
 from ..வடிவம் import உரை_வடிவம், தேவையான_வடிவங்கள்
 from ..மொழிபெயர்ப்பாளர்.மொழிபெயர்ப்பாளர் import மொழிபெயர்ப்பாளர்
 
@@ -14,5 +15,8 @@ class எண்(உரை):
 
     def வெளியிடு(தன், மொழி: str, மொழியாக்கம்: மொழிபெயர்ப்பாளர், வடிவங்கள்: தேவையான_வடிவங்கள்) -> Iterable[Span]:
         வடிவங்கள்.சேரு(தன்.வடிவம்)
-
-        yield Span(தன்.உரை, style=தன்.வடிவம்.பெயர்)
+        try:
+            உரை = உரைக்கு(தன்.உரை, மொழி=wuj.ruchabäl(மொழி))
+        except ValueError:
+            உரை = தன்.உரை
+        yield Span(உரை, style=தன்.வடிவம்.பெயர்)
