3ed5f7ef0490148144244f2439ee3068d30f427c
julienmalard <julien.malard@mail.mcgill.ca>
2025-04-01 12:37:29 +0200

    கட்டுரைகள்

--- src/தன்விவரங்கள்/உருப்படி/வெளியீடுகள்/வெளியீடு.py
+++ src/தன்விவரங்கள்/உருப்படி/வெளியீடுகள்/வெளியீடு.py
@@ -9,7 +9,5 @@ wuj = Wuj()
 
 
 class வெளியீடு(உருப்படி):
-    @abstractmethod
-    def வடிவூட்டம்(தன், மொழி: str, தகவல்கள்: dict[str, str], மொழிப்பெயர்பாளர்: மொழிபெயர்ப்பாளர்) -> str:
-        pass
+    pass
 
