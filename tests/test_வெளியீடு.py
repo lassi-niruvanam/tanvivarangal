f836e7a5d2339460c7482553901b0cae36e6e733
julienmalard <julien.malard@mail.mcgill.ca>
2025-04-23 23:11:37 +0200

    வெளியீடு கோப்புரை

--- tests/test_வெளியீடு.py
+++ tests/test_வெளியீடு.py
@@ -11,7 +11,7 @@ class செய்தி_உருவாக்கம்(unittest.TestCase):
         நான் = தன்விவரங்கள்("த")
         நான்.பெயர்("நான் தான்")
         with tempfile.TemporaryDirectory() as கோப்புரை:
-            நான்.வெளியிடு(வடிவூட்டம்="odt", கோப்பு=path.join(கோப்புரை, "தன்விவரங்கள்"))
+            நான்.வெளியிடு(வடிவூட்டம்="odt", கோப்புரை=கோப்புரை)
             தன்.assertTrue(os.path.isfile(path.join(கோப்புரை, "தன்விவரங்கள்_த.odt")))
 
     def test_pdf(தன்):
@@ -19,7 +19,7 @@ class செய்தி_உருவாக்கம்(unittest.TestCase):
         நான்.பெயர்("நான் தான்")
         with tempfile.TemporaryDirectory() as கோப்புரை:
             try:
-                நான்.வெளியிடு(வடிவூட்டம்="pdf", கோப்பு=path.join(கோப்புரை, "தன்விவரங்கள்"))
+                நான்.வெளியிடு(வடிவூட்டம்="pdf", கோப்புரை=கோப்புரை)
             except FileNotFoundError:
                 தன்.skipTest("soffice கிடைக்கவில்லை")
             தன்.assertTrue(os.path.isfile(path.join(கோப்புரை, "தன்விவரங்கள்_த.pdf")))
